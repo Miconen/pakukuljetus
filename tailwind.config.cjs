f78e8d8a65e1dd044d0f049e5348f45fa3ab918d
Miconen <micropizza.mc@gmail.com>
2023-05-05 20:58:58 +0300

    Add font family

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [],
+    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ["Lato", "Helvetica Neue", "Helvetica", "Arial", "sans-serif"],
+            },
+        },
+    },
+    plugins: [],
 }
