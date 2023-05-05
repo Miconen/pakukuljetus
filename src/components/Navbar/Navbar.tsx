de59899a786cc9e540f8229f92350c3c056f6670
Miconen <micropizza.mc@gmail.com>
2023-05-05 20:58:24 +0300

    Add navbar component

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,7 +1,16 @@
+import Container from "../Container/Container";
+import { RxHamburgerMenu } from 'react-icons/rx';
 import "./Navbar.scss";
 
 function Navbar() {
-    return;
+    return (
+        <nav className="flex justify-center w-full py-3 bg-white sticky top-0">
+            <Container className="items-center justify-between" horizontal={true}>
+                <img className="justify-items-start" src="https://www.pakukuljetus.net/wp-content/uploads/2021/04/pakukuljetus.webp" alt="Pakukuljetus logo" />
+                <RxHamburgerMenu className="w-8 h-auto"/>
+            </Container>
+        </nav>
+    );
 }
 
 export default Navbar;
