b413cad55922ffd77ab9ecb26aed458c0333efc9
Miconen <micropizza.mc@gmail.com>
2023-05-06 23:09:26 +0300

    Update Container style handling

--- src/components/Container/Container.tsx
+++ src/components/Container/Container.tsx
@@ -1,4 +1,5 @@
 import type { ReactElement } from "react";
+import classList from "../../styles/classList";
 
 type Props = {
     children?: ReactElement | ReactElement[];
@@ -7,8 +8,24 @@ type Props = {
 }
 
 function Container({children, className = "", horizontal}: Props) {
+    const containerStyles = {
+        base: "container flex px-4 md:px-0",
+        horizontal: "flex-row",
+        vertical: "flex-col",
+    };
+
+    // Create an array of classes
+    const classes = [containerStyles.base];
+
+    if (horizontal) classes.push(containerStyles.horizontal);
+    else classes.push(containerStyles.vertical);
+
+    if (className) classes.push(className);
+
+    const classListStr = classList(classes);
+
     return (
-        <div className={`${className} container flex ${horizontal ? "flex-row" : "flex-col"}`}>
+        <div className={classListStr}>
             {children}
         </div>
     )
