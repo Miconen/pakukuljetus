2c0f56541b1fb6478baab09d1f70bc33ad34bc4a
Miconen <micropizza.mc@gmail.com>
2023-05-06 23:48:36 +0300

    Seperate links to improve clickability area

--- src/components/Pages/Index/History.tsx
+++ src/components/Pages/Index/History.tsx
@@ -13,7 +13,7 @@ function History() {
                         <p>Olen <span className="font-bold">Marko Rintala</span>, Pakukuljetuksen perustaja ja nokkamies. Kun perustin firman, kaikki lähti liikkeelle melko vaatimattomasti — kokoonpanolla paku ja mies. Nyt meitä on jo useita ja pakun kaveriksi on saatu myös kuorma-auto, jonka avulla myös suurempien asuntojen muutot onnistuvat helposti.</p>
                         <p><span className="font-bold">Yrittäjänä minulle on tärkeää, että muuttosi onnistuu odotetusti.</span> Selkä suorana voin sanoa, että olemme onnistuneet tässä tavoitteessa – siitä osan voit lukea tämänkin sivun asiakaspalautteista. Tästä kiitos kuuluu tietysti kokeneille muuttomiehillemme, jotka hoitavat muutot ammattilaisen rutiinilla päivästä toiseen.</p>
                         <p><span className="font-bold">Jos haluat hyvää muuttopalvelua, soita minulle.</span> Lupaamme hoitaa muuttosi ammattitaidolla ja kohtuuhintaan.</p>
-                        <div>
+                        <div className="flex flex-col gap-4">
                             <p className="font-bold">Marko Rintala</p>
                             <Phone number="0400 631 118" call="+358400631118" />
                             <Email email="helsinki@pakukuljetus.fi" />
